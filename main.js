9eadc328c83c9bdd913684dbb2a1967bddce4adf
2109aishanidas <75248091+2109aishanidas@users.noreply.github.com>
2021-01-19 19:31:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+noseX=0
+noseY=0
+difference=0
+leftWristX=0
+rightWristX=0
+function setup(){
+    video=createCapture(VIDEO)
+    video.size(550,500)
+    canvas=createCanvas(550,500)
+    canvas.position(560,150)
+    poseNet=ml5.poseNet(video,modelLoaded)
+    poseNet.on('pose',gotPoses)
+    
+}
+function draw(){
+    background("rgb(147,156,151)")
+    textSize(difference)
+    document.getElementById("font_side").innerHTML="The Width And Height Of The Font ="+difference
+    text('Aishani Das', 60, 60);
+    fill("#CF78F7");
+}
+
+function modelLoaded(){
+    console.log('Model is Loaded')
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+        noseX=results[0].pose.nose.x
+        noseY=results[0].pose.nose.y
+        console.log("noseX"+noseX+"noseY"+noseY)
+        leftWristX=results[0].pose.leftWrist.x
+        rightWristX=results[0].pose.rightWrist.x
+        difference=floor(leftWristX-rightWristX)
+        console.log("leftWristX"+leftWristX+"rightWristX"+rightWristX+"difference"+difference)
+        
+    }
+}
